ef357cc262a7046295e4b8d33a4f84248c588ba9
Shawn Silverman <shawn@pobox.com>
2026-07-29 16:12:47 -0700

    project: Add Arduino library header for library inclusion

---
+++ src/imxrt1060_regs.h
@@ -0,0 +1,8 @@
+// SPDX-FileCopyrightText: (c) 2026 Shawn Silverman <shawn@pobox.com>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+// imxrt1060_regs.h is meant to be included by an Arduino project so that the
+// library gets included. It is intentionally empty.
+// This file is part of the imxrt1060-regs library.
+
+#pragma once
